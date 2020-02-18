a13f58488f4b8a62a004cc5c6f1007dbd281c07b
Jonny <jonathanomahony@gmail.com>
2020-02-18 20:18:17 +0000

    Fix up return case statement

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -4,9 +4,9 @@ pub fn raindrops(input: u32) -> String {
     if factor_of(input, 5) { result.push_str("Plang") };
     if factor_of(input, 7) { result.push_str("Plong") };
     if result.is_empty() { result = input.to_string() };
-    return result;
+    result
 }
 
 fn factor_of(input: u32, func: u32) -> bool {
-    return input % func == 0;
+    input % func == 0
 }
