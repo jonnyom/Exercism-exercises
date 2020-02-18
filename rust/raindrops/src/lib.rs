dd73c6ae9939a403b87fda6b085f46f73e18ef1a
Jonny <jonathanomahony@gmail.com>
2020-02-18 19:37:52 +0000

    Working raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,14 +1,12 @@
 pub fn raindrops(input: u32) -> String {
     let mut result = String::new();
-    if greater_than_0(input, 3) { result.push_str("pling") };
-    if greater_than_0(input, 5) { result.push_str("plang") };
-    if greater_than_0(input, 7) { result.push_str("plong") };
+    if factor_of(input, 3) { result.push_str("Pling") };
+    if factor_of(input, 5) { result.push_str("Plang") };
+    if factor_of(input, 7) { result.push_str("Plong") };
     if result.is_empty() { result = input.to_string() };
     return result;
 }
 
-fn greater_than_0(input: u32, func: u32) -> bool {
-    let greater = input % func >= 0;
-    println!("input: {:?} func: {:?} greater: {:?}", input, func, greater);
-    return greater;
+fn factor_of(input: u32, func: u32) -> bool {
+    return input % func == 0;
 }
