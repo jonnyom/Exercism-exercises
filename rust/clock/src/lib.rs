461cc7db1f353cbf8318fd64d84002cced466fb3
Jonny <jonathanomahony@gmail.com>
2020-03-10 14:27:32 +0000

    Clock exercise

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,23 @@
+use std::fmt;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Clock(i32, i32);
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        Clock(
+            (hours + minutes.div_euclid(60)).rem_euclid(24),
+            minutes.rem_euclid(60)
+        )
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(self.0, self.1 + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      write!(f, "{:0>2}:{:0>2}", self.0, self.1)
+    }
+}
