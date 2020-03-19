f7c822387316d42ed4eeb11c80488093225025af
Jonny <jonathanomahony@gmail.com>
2020-03-19 20:32:58 +0000

    It likes more tabs

--- rust/simple-linked-list/src/lib.rs
+++ rust/simple-linked-list/src/lib.rs
@@ -12,59 +12,62 @@ struct Node<T> {
 }
 
 impl<T> SimpleLinkedList<T> {
-	pub fn new() -> Self {
-		Self { head: None }
-	}
+    pub fn new() -> Self {
+        Self { head: None }
+    }
 
-	pub fn len(&self) -> usize {
-		let mut count: usize = 0;
-		let mut node = &(self.head);
-		while node.is_some() {
-			count += 1;
-			node = &(node.as_ref().unwrap().next);
-		}
-		count
-	}
+    pub fn len(&self) -> usize {
+        let mut count: usize = 0;
+        let mut node = &(self.head);
+        while node.is_some() {
+            count += 1;
+            node = &(node.as_ref().unwrap().next);
+        }
+        count
+    }
 
-	pub fn push(&mut self, _element: T) {
-		let node = Box::new(Node { data: _element, next: self.head.take() });
-		self.head = Some(node);
-	}
+    pub fn push(&mut self, _element: T) {
+        let node = Box::new(Node {
+            data: _element,
+            next: self.head.take(),
+        });
+        self.head = Some(node);
+    }
 
-	pub fn pop(&mut self) -> Option<T> {
-		match self.head.take() {
-			None => None,
-			Some(node) => {
-				self.head = node.next;
-				Some(node.data)
-			}
-		}
-	}
+    pub fn pop(&mut self) -> Option<T> {
+        match self.head.take() {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.data)
+            }
+        }
+    }
 
-	pub fn peek(&self) -> Option<&T> {
-		match self.head {
-			None => None,
-			Some(ref node) => Some(&node.data)
-		}
-	}
+    pub fn peek(&self) -> Option<&T> {
+        match self.head {
+            None => None,
+            Some(ref node) => Some(&node.data),
+        }
+    }
 
-	pub fn rev(mut self) -> SimpleLinkedList<T> {
-		let mut rev_ll = Self::new();
-		while self.head.is_some() {
-			rev_ll.push(self.pop().unwrap());
-		}
-		rev_ll
-	}
+    pub fn rev(mut self) -> SimpleLinkedList<T> {
+        let mut rev_ll = Self::new();
+        while self.head.is_some() {
+            rev_ll.push(self.pop().unwrap());
+        }
+        rev_ll
+    }
 }
 
 impl<T> FromIterator<T> for SimpleLinkedList<T> {
-	fn from_iter<I: IntoIterator<Item = T>>(_iter: I) -> Self {
-		let mut ll = SimpleLinkedList::new();
-		for elem in _iter {
-			ll.push(elem);
-		}
-		ll
-	}
+    fn from_iter<I: IntoIterator<Item = T>>(_iter: I) -> Self {
+        let mut ll = SimpleLinkedList::new();
+        for elem in _iter {
+            ll.push(elem);
+        }
+        ll
+    }
 }
 
 // In general, it would be preferable to implement IntoIterator for SimpleLinkedList<T>
@@ -79,11 +82,11 @@ impl<T> FromIterator<T> for SimpleLinkedList<T> {
 // demands more of the student than we expect at this point in the track.
 
 impl<T> Into<Vec<T>> for SimpleLinkedList<T> {
-	fn into(mut self) -> Vec<T> {
-		let mut vec = Vec::new();
-		while self.head.is_some() {
-			vec.insert(0, self.pop().unwrap());
-		}
-		vec
-	}
+    fn into(mut self) -> Vec<T> {
+        let mut vec = Vec::new();
+        while self.head.is_some() {
+            vec.insert(0, self.pop().unwrap());
+        }
+        vec
+    }
 }
